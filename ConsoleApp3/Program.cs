76f9429606e5601ebdb97bd51ca5f6983aac310f
MrTrojan7 <yur4ik.karataev@gmail.com>
2022-04-30 11:27:29 +0300

    add stars

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Stairs
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Input quantity of stairs: ");
+            int n = Convert.ToInt32(System.Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int spaces = 2 * i;
+                for (int j = 0; j < spaces; j++)
+                    Console.Write(' ');
+                Console.WriteLine("***");
+                spaces = 2 * (i + 1);
+                for (int j = 0; j < spaces; j++)
+                    Console.Write(' ');
+                Console.WriteLine("*");
+            }
+        }
+    }
+}
