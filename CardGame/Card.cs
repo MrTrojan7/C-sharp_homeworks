24fa7b133077d1d722b36b6190d982e4dec299be
MrTrojan7 <yur4ik.karataev@gmail.com>
2022-07-11 13:34:00 +0300

    add methods

--- CardGame/Card.cs
+++ CardGame/Card.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CardGame
 {
-    internal class Card
+    class Card
     {
+        private string value;
+        private char type;
+        private int power;
+
+        public Card(string value, char type, int power)
+        {
+            this.value = value;
+            this.type = type;
+            this.power = power;
+        }
+        
+        public void ShowCard()
+        {
+            Console.Write("< " + this.value + this.type + " >");
+        }
+        
+        public int Power
+        {
+            get { return this.power; }
+        }
     }
 }
