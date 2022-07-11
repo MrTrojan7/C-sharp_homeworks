de8990babadd59136b7f5a08acd23360c22f9790
MrTrojan7 <yur4ik.karataev@gmail.com>
2022-07-11 12:57:06 +0300

    add main file

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
