76d4c17d7dbf6def9c101e958b7fe3349b0600f1
MrTrojan7 <yur4ik.karataev@gmail.com>
2022-07-15 16:38:02 +0300

    task 1 is done

--- FileStream/Program.cs
+++ FileStream/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DirectoryInfoExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Now";
+            DirectoryInfo root = new DirectoryInfo(path);
+            FileInfo[] files = root.GetFiles();
+            foreach (FileInfo f in files)
+            {
+                FileStream fs = new FileStream(path + @"\" + f.Name, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+                if (f.Name.Substring(f.Name.LastIndexOf('.')) == ".txt")
+                {
+                    string file_text = sr.ReadToEnd();
+                    if (file_text.Contains("sharp"))
+                    {
+                        Console.WriteLine(f);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
