fd5efb5fc7403f68fe7365cbff04dc6823eb3cc3
MrTrojan7 <yur4ik.karataev@gmail.com>
2022-07-17 18:38:38 +0300

    add ClassLibrary

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace UsingCppLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CppFunction();
+        }
+
+        [DllImport("E:/Step/.NET/C#/DLL1.dll")]
+        static extern void CppFunction();
+    }
+}
