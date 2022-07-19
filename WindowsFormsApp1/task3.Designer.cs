15e0fbce7b662e2417f53e0f76bc3280a63f5bfb
MrTrojan7 <yur4ik.karataev@gmail.com>
2022-07-19 18:01:46 +0300

    task3 is done

---
+++ WindowsFormsApp1/task3.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class task3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.driving_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            this.timer.Start();
+            // 
+            // driving_button
+            // 
+            this.driving_button.Location = new System.Drawing.Point(37, 62);
+            this.driving_button.Name = "driving_button";
+            this.driving_button.Size = new System.Drawing.Size(75, 23);
+            this.driving_button.TabIndex = 0;
+            this.driving_button.Text = "Я уезжаю";
+            this.driving_button.UseVisualStyleBackColor = true;
+            // 
+            // task3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 336);
+            this.Controls.Add(this.driving_button);
+            this.Name = "task3";
+            this.Text = "task3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button driving_button;
+    }
+}
---
+++ WindowsFormsApp1/task3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class task3 : Form
+    {
+        Point templocation = new Point(37, 62);
+
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+
+        public task3()
+        {
+            InitializeComponent();
+            timer.Tick += Timer_Tick;
+            timer.Interval = 50;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            this.driving_button.Location = templocation;
+            templocation.X += 2;
+        }
+    }
+}
