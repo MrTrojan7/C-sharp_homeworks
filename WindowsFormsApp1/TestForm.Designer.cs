b8717b8119c1f8072e0af18275ea89d1d7f03b18
MrTrojan7 <yur4ik.karataev@gmail.com>
2022-07-19 14:33:21 +0300

    add images

--- WindowsFormsApp1/TestForm.Designer.cs
+++ WindowsFormsApp1/TestForm.Designer.cs
@@ -28,32 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.authorization = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // panel1
             // 
-            this.button1.Location = new System.Drawing.Point(94, 49);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(79, 50);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "TestButton1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(215)))), ((int)(((byte)(240)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.authorization);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(370, 88);
+            this.panel1.TabIndex = 1;
+            // 
+            // authorization
+            // 
+            this.authorization.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.authorization.Font = new System.Drawing.Font("Tempus Sans ITC", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.authorization.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(121)))), ((int)(((byte)(123)))));
+            this.authorization.Location = new System.Drawing.Point(0, 0);
+            this.authorization.Name = "authorization";
+            this.authorization.Size = new System.Drawing.Size(370, 88);
+            this.authorization.TabIndex = 0;
+            this.authorization.Text = "Authorization";
+            this.authorization.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(356, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
             // 
             // TestForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(111)))), ((int)(((byte)(126)))));
             this.ClientSize = new System.Drawing.Size(370, 310);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Name = "TestForm";
             this.Text = "TestForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label authorization;
+        private System.Windows.Forms.Label label1;
     }
 }
