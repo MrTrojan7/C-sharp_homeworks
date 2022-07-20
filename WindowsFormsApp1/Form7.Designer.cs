b7aff55cae9cbe38339f29fec57969a932392eb5
MrTrojan7 <yur4ik.karataev@gmail.com>
2022-07-20 14:26:50 +0300

    task7 is done

---
+++ WindowsFormsApp1/Form7.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonToPress = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonToPress
+            // 
+            this.buttonToPress.BackColor = System.Drawing.Color.GreenYellow;
+            this.buttonToPress.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonToPress.FlatAppearance.BorderColor = System.Drawing.Color.Goldenrod;
+            this.buttonToPress.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow;
+            this.buttonToPress.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.buttonToPress.Font = new System.Drawing.Font("Monotype Corsiva", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonToPress.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonToPress.Location = new System.Drawing.Point(205, 123);
+            this.buttonToPress.Name = "buttonToPress";
+            this.buttonToPress.Size = new System.Drawing.Size(378, 151);
+            this.buttonToPress.TabIndex = 0;
+            this.buttonToPress.Text = "Move the mouse over the button and press it immediately";
+            this.buttonToPress.UseVisualStyleBackColor = false;
+            this.buttonToPress.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonToPress_MouseClick);
+            this.buttonToPress.MouseLeave += new System.EventHandler(this.buttonToPress_MouseLeave);
+            this.buttonToPress.MouseMove += new System.Windows.Forms.MouseEventHandler(this.buttonToPress_MouseMove);
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonToPress);
+            this.Name = "Form7";
+            this.Text = "Clicker";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonToPress;
+    }
+}
---
+++ WindowsFormsApp1/Form7.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form7 : Form
+    {
+        private static Timer vTimer = new Timer();
+        private static TimeSpan time_span = new TimeSpan();
+        private static DateTime date_end;
+        private bool flag_of_move = false;
+        private int count_of_click = 0;
+        private int max_cnt = 0;
+
+        /// <summary>
+        /// show time on button 
+        /// if time is out - end of round
+        /// </summary>
+        private void ShowTime(object vObj, EventArgs e)
+        {
+            time_span = date_end - DateTime.Now;
+            this.buttonToPress.Text = time_span.Seconds.ToString();
+            if (time_span.TotalSeconds < 0)
+            {
+                EndOfRound();
+            }
+        }
+        /// <summary>
+        /// end of round
+        /// show results
+        /// </summary>
+        private void EndOfRound()
+        {
+            vTimer.Stop();
+            if (count_of_click > max_cnt)
+            {
+                max_cnt = count_of_click;
+            }
+            this.buttonToPress.Text = "End! Your count of click: " + count_of_click.ToString() +
+                ". Max count: " + max_cnt.ToString() +
+                ".\nTake the mouse off the button and hover again to start a new round";
+            count_of_click = 0;
+        }
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public Form7()
+        {
+            InitializeComponent();
+            vTimer.Tick += new EventHandler(ShowTime);
+            vTimer.Interval = 1000;
+        }
+
+        /// <summary>
+        /// Start round
+        /// </summary>
+        private void buttonToPress_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!flag_of_move)
+            {
+                date_end = DateTime.Now.AddSeconds(20);
+                this.buttonToPress.Text = "Start now!";
+                vTimer.Start();
+                flag_of_move = true;
+            }
+        }
+
+        /// <summary>
+        /// Counter of clicks (if round is start)
+        /// </summary>
+        private void buttonToPress_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (flag_of_move)
+            {
+                ++count_of_click;
+            }
+        }
+
+        /// <summary>
+        /// round end logic (if time not out - send message "try again")
+        /// </summary>
+        private void buttonToPress_MouseLeave(object sender, EventArgs e)
+        {
+            if (vTimer.Enabled)
+            {
+                this.buttonToPress.Text = "You took the mouse off the button.\nTry again";
+            }
+            if (flag_of_move)
+            {
+                flag_of_move = false;
+            }
+            vTimer.Stop();
+        }
+    }
+}
