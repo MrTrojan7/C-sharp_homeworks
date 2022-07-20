7f66fa0c285127b7934def8a3d6251aafc5874d0
MrTrojan7 <yur4ik.karataev@gmail.com>
2022-07-20 13:08:45 +0300

    add version 2

--- WindowsFormsApp1/Form6.cs
+++ WindowsFormsApp1/Form6.cs
@@ -12,12 +12,16 @@ namespace WindowsFormsApp1
 {
     public partial class Form6 : Form
     {
-        private Color[] myColor = new Color[] 
-        { Color.Black, Color.Red, Color.Yellow, Color.Green,
-        Color.Azure, Color.Blue, Color.Pink, Color.White };
-        private static Timer vTimer = new Timer();
-        private static short count = 0;
-        private void ShowColor(object vObj, EventArgs e)
+        private Color[] myColor = new Color[] // version 1
+        { 
+            Color.Black, Color.Red, Color.Yellow, Color.Green,
+            Color.Azure, Color.Blue, Color.Pink, Color.White 
+        };
+
+        private static Timer vTimer = new Timer(); // all versions
+        private static short count = 0; // version 1
+        private static Random rnd = new Random(); //version 2
+        private void ShowColor(object vObj, EventArgs e) // version 1
         {
             if (count >= myColor.Length)
             {
@@ -26,11 +30,17 @@ namespace WindowsFormsApp1
             this.BackColor = myColor[count];
             ++count;
         }
+
+        private void ShowColor_v2(object vObj, EventArgs e) // version 2
+        {
+            this.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+        }
+
         public Form6()
         {
             InitializeComponent();
-            vTimer.Tick += new EventHandler(ShowColor);
-            vTimer.Interval = 500;
+            vTimer.Tick += new EventHandler(ShowColor); // replace the function (version 1/2)
+            vTimer.Interval = 500; 
             vTimer.Start();
         }
     }
