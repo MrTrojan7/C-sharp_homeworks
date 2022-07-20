8c1485735047386e4734ddaa97745f637c50c43b
MrTrojan7 <yur4ik.karataev@gmail.com>
2022-07-20 12:08:24 +0300

    task6 is done

---
+++ WindowsFormsApp1/Form6.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form6 : Form
+    {
+        private Color[] myColor = new Color[] 
+        { Color.Black, Color.Red, Color.Yellow, Color.Green,
+        Color.Azure, Color.Blue, Color.Pink, Color.White };
+        private static Timer vTimer = new Timer();
+        private static short count = 0;
+        private void ShowColor(object vObj, EventArgs e)
+        {
+            if (count >= myColor.Length)
+            {
+                count = 0;
+            }
+            this.BackColor = myColor[count];
+            ++count;
+        }
+        public Form6()
+        {
+            InitializeComponent();
+            vTimer.Tick += new EventHandler(ShowColor);
+            vTimer.Interval = 500;
+            vTimer.Start();
+        }
+    }
+}
